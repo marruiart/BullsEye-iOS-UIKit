9824e84f11feef09cce777bc4f1c91da68365944
Marina Ruiz <marina.ruiz.dev@gmail.com>
2024-02-21 16:56:39 +0100

    Add slider and generate random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,17 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion:nil)
+        startNewRound()
     }
 }
-
