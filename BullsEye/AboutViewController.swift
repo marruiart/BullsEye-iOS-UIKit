b72012dedda262ece89ac4a3a040144d863fca3c
Marina Ruiz <marina.ruiz.dev@gmail.com>
2024-02-22 10:15:21 +0100

    Create information view

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Marina on 22/2/24.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
