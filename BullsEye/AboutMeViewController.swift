8fb8d3e56fab85c8a2d937cc218f7cf06f1e46e5
Marina Ruiz <marina.ruiz.dev@gmail.com>
2024-02-22 11:53:43 +0100

    Create author view

---
+++ BullsEye/AboutMeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutMeViewController.swift
+//  BullsEye
+//
+//  Created by Marina on 22/2/24.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    @IBAction func onClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
