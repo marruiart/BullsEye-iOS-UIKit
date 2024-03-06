ba16e9907a8b1389416df25044371968ef14d1c4
Marina Ruiz <marina.ruiz.dev@gmail.com>
2024-03-06 09:54:21 +0100

    Add webview to about page

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
@@ -16,6 +18,13 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
 
 }
